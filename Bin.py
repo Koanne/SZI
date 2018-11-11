614204161b1dd62780ad719ba2ce059e8a0143f0
Koanne <32874960+Koanne@users.noreply.github.com>
2018-11-11 23:53:44 +0100

    First commit

---
+++ Bin.py
@@ -0,0 +1,13 @@
+from MapElement import MapElement
+
+class Bin(object):
+    def __init__(self, x, y):
+        self.position = [x,y]
+        self.color = 'red'
+        self.state = 'full'
+
+    def action(self):
+        pass
+
+    def display(self):
+        pass
---
+++ MapElement.py
@@ -0,0 +1,11 @@
+class MapElement(object):
+
+    def __init__(self, x, y):
+        self.position = [x, y]
+        self.color = 'white'
+
+    def action(self):
+        pass
+
+    def display(self):
+        pass
