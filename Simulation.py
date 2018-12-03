5e39504486fe2dba38bdec1a552e6bbfee6d80f0
jarosy <jarosyjarosy@gmail.com>
2018-12-03 23:52:55 +0100

    getPriority popawione

--- Simulation.py
+++ Simulation.py
@@ -127,4 +127,4 @@ class Simulation(object):
         return False
 
     def getPriority(self, pos1, pos2)
-        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos[2])
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
